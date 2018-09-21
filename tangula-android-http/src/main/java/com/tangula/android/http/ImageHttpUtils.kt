a6fa44d080be6427a3114302a0c29b6960452d0b
临碣 <wangds2016@163.com>
2018-09-21 14:51:22 +0800

    显示图片PlaceHolder支持资源Id

--- tangula-android-http/src/main/java/com/tangula/android/http/ImageHttpUtils.kt
+++ tangula-android-http/src/main/java/com/tangula/android/http/ImageHttpUtils.kt
@@ -8,7 +8,6 @@ import com.jakewharton.picasso.OkHttp3Downloader
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import com.tangula.android.http.HttpBase.Companion.postBizSucessA
-import com.tangula.android.utils.UiThreadUtils
 import com.tangula.utils.BitmapUtils
 import com.tangula.utils.function.Consumer
 import okhttp3.Interceptor
@@ -31,6 +30,27 @@ class ImageHttpUtils {
             }
         }
 
+
+        @JvmStatic
+        fun loadImage(view: ImageView, url: String, placeHolder: Int?, errorHolder: Int?) {
+            loadImage(view.context, view, url, placeHolder, errorHolder, Runnable {})
+        }
+
+        @JvmStatic
+        fun loadImage(view: ImageView, url: String, placeHolder: Int?, errorHolder: Int?, onSuccess: Runnable) {
+            loadImage(view.context, view, url, placeHolder, errorHolder, onSuccess)
+        }
+
+        @JvmStatic
+        fun loadImage(view: ImageView, url: String, placeHolder: Int?, errorHolder: Int?, onSuccess: Runnable, onFail: Runnable) {
+            loadImage(view.context, view, url, placeHolder, errorHolder, onSuccess, onFail)
+        }
+
+        @JvmStatic
+        fun loadImage(context: Context?, view: ImageView, url: String, placeHolder: Int?, errorHolder: Int?, onSuccess: Runnable) {
+            loadImage(context, view, url, placeHolder, errorHolder, onSuccess, Runnable {})
+        }
+
         @JvmStatic
         fun loadImage(view: ImageView, url: String, placeHolder: Drawable?, errorHolder: Drawable?) {
             loadImage(view.context, view, url, placeHolder, errorHolder, Runnable {})
@@ -49,6 +69,52 @@ class ImageHttpUtils {
         @JvmStatic
         fun loadImage(context: Context?, view: ImageView, url: String, placeHolder: Drawable?, errorHolder: Drawable?, onSuccess: Runnable) {
             loadImage(context, view, url, placeHolder, errorHolder, onSuccess, Runnable {})
+        }
+
+        /**
+         * 显示图片.
+         */
+        @JvmStatic
+        fun loadImage(context: Context?, view: ImageView, url: String, placeHolder: Int?, errorHolder: Int?, onSuccess: Runnable, onFail: Runnable) {
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(object : Interceptor {
+                        @Throws(IOException::class)
+                        override fun intercept(chain: Interceptor.Chain): Response {
+                            val newRequest = chain.request().newBuilder()
+                                    .addHeader("auth", HttpBase.USER_ID_SUPPLIER.get())
+                                    .build()
+                            return chain.proceed(newRequest)
+                        }
+                    })
+                    .build()
+
+            val picasso = Picasso.Builder(context ?: view.context)
+                    .downloader(OkHttp3Downloader(client))
+                    .build()
+
+            val req = picasso.load(url)
+            when (placeHolder != null) {
+                true -> {
+                    req.placeholder(placeHolder!!)
+                }
+            }
+            when (errorHolder != null) {
+                true -> {
+                    req.error(errorHolder!!)
+                }
+            }
+
+            view.post{
+                req.into(view, object : Callback {
+                    override fun onSuccess() {
+                        onSuccess.run()
+                    }
+
+                    override fun onError() {
+                        onFail.run()
+                    }
+                })
+            }
         }
 
         /**
